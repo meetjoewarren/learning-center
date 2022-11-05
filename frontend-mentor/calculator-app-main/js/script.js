bcea60a373a46533448123c19ea95dd8be993ba0
meetjoewarren <meetjoewarren@gmail.com>
2022-11-05 16:48:36 -0700

    Front-End Mentor Challenges

--- frontend-mentor/calculator-app-main/js/script.js
+++ frontend-mentor/calculator-app-main/js/script.js
@@ -1,15 +1,24 @@
 const keypad = document.querySelector('.keypad');
 const display = document.querySelector('.display');
+const operator = document.querySelectorAll('.operator');
 
 keypad.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-        const key = e.target;
-        const keyInput = key.textContent;
-        if (Number(display.textContent) >= 0 || Number(display.textContent) <= 9) {
-            display.textContent = keyInput;
-        }
-        else { return; }
+  if (e.target.matches('button')) {
+    const key = e.target;
+    const keyInput = key.textContent;
 
-        console.log(keyInput)
+    if (Number(key.textContent) >= 0 || Number(key.textContent) <= 9) {
+      display.textContent += keyInput;
+      console.log('key ' + keyInput);
     }
+  }
+
+  if (e.target.matches('button.key-del')) {
+    display.textContent = display.textContent.slice(0, -1);
+    console.log('del');
+  }
+
+  if (e.target.matches('button.key-plus')) {
+    const numOne = Number(display.textContent) + 
+  }
 })
