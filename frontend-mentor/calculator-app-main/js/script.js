03cfd0c13e91716bceaa3b985583484f27678dc1
meetjoewarren <meetjoewarren@gmail.com>
2022-11-05 10:54:51 -0700

    Front-End Mentor Challenge

--- frontend-mentor/calculator-app-main/js/script.js
+++ frontend-mentor/calculator-app-main/js/script.js
@@ -1,8 +1,15 @@
 const keypad = document.querySelector('.keypad');
+const display = document.querySelector('.display');
 
-keypad.addEventListener('click', e=> {
+keypad.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target;
-        
+        const keyInput = key.textContent;
+        if (Number(display.textContent) >= 0 || Number(display.textContent) <= 9) {
+            display.textContent = keyInput;
+        }
+        else { return; }
+
+        console.log(keyInput)
     }
 })
